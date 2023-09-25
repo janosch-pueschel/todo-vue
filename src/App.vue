b68c0ee66a12c46e04bd572f8861f35f20e37223
Janosch Püschel <janosch.pueschel@gmail.com>
2023-09-25 14:22:50 +0200

    add function to add todo item

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,52 @@
-<script>
+<script setup>
+import { ref } from "vue";
+import { nanoid } from "nanoid";
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiPlus } from "@mdi/js";
 import Todos from "./components/Todos.vue";
-export default {
-  name: "App",
-  components: {
-    Todos,
-  },
-};
+
+const todos = ref([]);
+const userInput = ref("");
+function addTodo() {
+  if (userInput.value === "") {
+    alert("Please enter your todo-item!");
+  } else {
+    const newTodoItem = {
+      text: userInput.value,
+      completed: false,
+      id: nanoid(),
+    };
+    todos.value.unshift(newTodoItem);
+    userInput.value = "";
+  }
+}
 </script>
 
 <template>
   <div>
-    <Todos />
+    <!-- <Header /> -->
+    <div class="flex flex-col items-center">
+      <div class="w-4/6 my-5 flex justify-center">
+        <div class="flex w-fit p-1 border-b border-slate-200 text-lg">
+          <input
+            class="appearance-none focus:outline-none w-96 mr-5"
+            type="text"
+            placeholder="Add Todo"
+            v-model="userInput"
+            @keydown.enter="addTodo"
+          />
+          <svg-icon type="mdi" :path="mdiPlus" @click="addTodo"></svg-icon>
+        </div>
+      </div>
+    </div>
+    <div class="w-4/6 mt-10">
+      <Todos
+        v-for="todo in todos"
+        :text="todo.text"
+        :completed="todo.completed"
+        :key="todo.id"
+        :number="todo.id"
+      />
+    </div>
   </div>
 </template>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,11 +1,17 @@
-<script>
-export default {
-  data() {
-    return {};
-  },
-  methods: {},
-};
+<script setup>
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiCheckCircleOutline } from "@mdi/js";
+import { mdiCheckCircle } from "@mdi/js";
+import { mdiDelete } from "@mdi/js";
+import { mdiPencil } from "@mdi/js";
+defineProps(["text", "completed", "key"]);
 </script>
 
 <template>
+  <div class="grid grid-cols-todos gap-1 mb-5 pb-5 border-b border-slate-200">
+    <svg-icon type="mdi" :path="mdiCheckCircleOutline"></svg-icon>
+    <p>{{ text }}</p>
+    <svg-icon type="mdi" :path="mdiPencil"></svg-icon>
+    <svg-icon type="mdi" :path="mdiDelete"></svg-icon>
+  </div>
 </template>
